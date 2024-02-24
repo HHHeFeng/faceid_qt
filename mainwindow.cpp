170b85c13e21d4dd3de6dd99ee613f4d39314e7e
HHHeFeng <hhhefeng@qq.com>
2024-02-24 18:08:18 +0800

    test_1_____1

---
+++ mainwindow.cpp
@@ -0,0 +1,23 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2\face.hpp>
+using namespace cv;
+
+MainWindow::MainWindow(QWidget* parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    Mat image = imread("D:/image/test.jpg", 3);
+    namedWindow("Display window", WINDOW_AUTOSIZE);
+    imshow("Display window", image);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
